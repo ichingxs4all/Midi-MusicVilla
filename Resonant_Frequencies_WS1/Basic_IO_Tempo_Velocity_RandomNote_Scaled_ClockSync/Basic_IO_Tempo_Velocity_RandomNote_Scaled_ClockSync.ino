2c013c5ab16edd12779f4c0a205799ee47d7bcc8
ichingxs4all <mragutlich@gmail.com>
2025-11-06 12:35:40 +0100

    More examples added

---
+++ Resonant_Frequencies_WS1/Basic_IO_Tempo_Velocity_RandomNote_Scaled_ClockSync/Basic_IO_Tempo_Velocity_RandomNote_Scaled_ClockSync.ino
@@ -0,0 +1,75 @@
+
+// Simple example of playing a fixed MIDI note in a tempo based on the level of a potentiometer ( sensor);
+//by Michel Gutlich for the Resonant Frequencies workshop #1
+
+#define LED_BUILTIN 3.  // A led is attached to pin 3 to have some visual indication of what is happening
+
+// We are goin to use the FortySevenEffects MIDI library. Make sure it downloaded and installed through the Library Manager
+//https://github.com/FortySevenEffects/arduino_midi_library
+#include <MIDI.h>
+
+MIDI_CREATE_DEFAULT_INSTANCE();  //Create a standard instance of a hardware Serial Port MIDI device
+
+int channel = 14;  //The MIDI channel we are going to send messages to. Make this the channel number your synthesizer is set on. Channel numbers are between 1 and 16
+
+int note = 42;  //The note number we are going send. Note numbers are between 0 and 127
+
+int velocity = 100;  //The velocity ( volume) of the note we are going to send. Velocty value are between 0 and 127
+
+int velScale = 4;  // scale factor for the velocity readings
+
+bool debug = true;  //Set this to false to turn off debugging to the USB serial port
+
+int numNotesInScale;  //This will hold the number of notes in a scale
+
+int scale = 1;  //This parameter will set the type of scale , 0 = chromatic, 1 = pentatatonic and 2 = blues scale
+
+int playing;  //This will hold a temporary value of the note  that is selected for playing after mapping it to a scale
+
+int step = 8;  //MIDI clock is defined at 24 'clicks' for a quarter note. This is the number of 'clicks' that stands for a step
+
+int click = 0;  // This will hold the MIDI clock clicks count
+
+int playStatus = 1; // //This is our play status ; 0 = stopped , 1 = playing , 2 = continue ( does not reset the click count, so start were it left off)
+
+int gateLength = 50; //This holds the gate length of the notes we are playing
+
+int lastNotePlayed; //This will hold the last note played
+
+long clock_timer; //This will store our clock timer value
+
+int transpose = 24; //The amount of notes transposed according to the root note
+
+void setup() {
+  if (debug) Serial.begin(115200);  //Setup the USB serial port for debugging if the boolean 'debug' is true
+
+  pinMode(LED_BUILTIN, OUTPUT);  //Set the pin where the led is attached to as an output
+
+  // Connect the handleNoteOn function to the library,
+  // so it is called upon reception of a NoteOn.
+  MIDI.setHandleNoteOn(handleNoteOn);  // Put only the name of the function
+
+  // Do the same for NoteOffs
+  MIDI.setHandleNoteOff(handleNoteOff);
+  // Connect the handleControlChange function to the library, so it is called upon reception of a ControlChange.
+  MIDI.setHandleControlChange(handleControlChange);
+  //The same for the reception of clock, start, stop and continue
+  MIDI.setHandleClock(handleClock);
+  MIDI.setHandleStart(handleStart);
+  MIDI.setHandleContinue(handleContinue);
+  MIDI.setHandleStop(handleStop);
+
+  MIDI.begin(MIDI_CHANNEL_OMNI);  // Launch MIDI and listen on all channels
+}
+
+void loop() {
+  
+  MIDI.read();  // Listen for incomming MIDI messages. It's a good thing to always implement this function in your own code
+
+  //Because we don't want to miss any clock pulses we make sure the last played note is turned off after the amount off gateLength
+  if ((clock_timer > 0) && (millis() - clock_timer >= gateLength)) {
+    handleNoteOff(channel, lastNotePlayed, 0); //Send note off for the last played note
+    digitalWrite(LED_BUILTIN, LOW);  //Turn off the led
+    clock_timer = 0; //Reset the clock timer
+  }
+}
---
+++ Resonant_Frequencies_WS1/Basic_IO_Tempo_Velocity_RandomNote_Scaled_Sync_CC/MidiCallBacks.ino
@@ -0,0 +1,69 @@
+// -----------------------------------------------------------------------------
+
+// This function will be automatically called when a NoteOn is received.
+// It must be a void-returning function with the correct parameters,
+// see documentation here:
+// https://github.com/FortySevenEffects/arduino_midi_library/wiki/Using-Callbacks
+
+void handleNoteOn(byte channel, byte note, byte velocity) {
+  // Do whatever you want when a note is pressed.
+  digitalWrite(LED_BUILTIN, HIGH);
+  // if(debug) Serial.println(note); //Send the value to the USB Serial portfor debugging if the boolean 'debug' is true
+  MIDI.sendNoteOn(note, velocity, channel);  // Send the note with a velocity on a channel
+
+  // Try to keep your callbacks short (no delays ect)
+  // otherwise it would slow down the loop() and have a bad impact
+  // on real-time performance.
+}
+
+void handleNoteOff(byte channel, byte note, byte velocity) {
+  MIDI.sendNoteOff(note, 0, channel);  // Stop the note
+  digitalWrite(LED_BUILTIN, LOW);      //Turn off the led
+}
+
+void handleControlChange(byte channel, byte number, byte value) {
+}
+
+void handleClock() {
+  if (playStatus == 1) {
+    
+ if(ccValue != lastCcValue){
+    MIDI.sendControlChange(ccNo, ccValue, channel);
+    lastCcValue = ccValue;
+  }
+
+    if (click == step) {
+
+      velocity = analogRead(A1) / velScale;  // Read analog port A1 and put the value in the parameter velocity. The value will be between 0 and 1023 zo we have to divide it by a scale factor to be in range
+
+      note = setScale(random(12), scale, transpose);  //Choose a random note number between 0 and 127, scale it and transpose it
+
+      handleNoteOn(channel, note, velocity);  //Play the calculated note
+
+      clock_timer = millis();  //Store the current time in milliseconds in clock_timer to compare it later with the elapsed time to
+
+      lastNotePlayed = note;  // Remember the last played note
+
+      click = 0;  //Reset the click counter
+    }
+
+    click++;  //Advance the click counter by 1
+  }
+}
+
+void handleStart() {
+  playStatus = 1;  //Set the play status to 1 ( playing)
+  if (debug) Serial.println("Playing");
+}
+
+void handleStop() {
+  playStatus = 0;  //Set the play status to 0 ( stopped)
+  click = 0;       //Reset the click counter -> If we restart we start from the beginning
+  if (debug) Serial.println("Stopped");
+}
+
+
+void handleContinue() {
+  playStatus = 2;  //Set the play status to 2 ( continue), we start were we left off
+  if (debug) Serial.println("Continu");
+}
